d4a5c8d67f18dd6ad48ff50143b8f79f316595d3
flatypus [mac] <yhc3141@gmail.com>
2024-12-01 18:49:59 -0800

    perf: don't think i'm improving this any more

--- src/day1.rs
+++ src/day1.rs
@@ -1,19 +1,20 @@
+use std::collections::HashMap;
+
 fn split_input(input: &str) -> (Vec<i32>, Vec<i32>) {
-    let (left, right): (Vec<i32>, Vec<i32>) = input
+    input
         .lines()
         .map(|line| {
             let mut nums = line.split_whitespace().map(|x| x.parse::<i32>().unwrap());
             (nums.next().unwrap(), nums.next().unwrap())
         })
-        .unzip();
-    (left, right)
+        .unzip()
 }
 
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> i32 {
     let (mut left, mut right) = split_input(input);
-    left.sort();
-    right.sort();
+    left.sort_unstable();
+    right.sort_unstable();
     let sum: i32 = left
         .iter()
         .zip(right.iter())
@@ -25,15 +26,13 @@ pub fn part1(input: &str) -> i32 {
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> i32 {
     let (left, right) = split_input(input);
-    let sum: i32 = left
-        .iter()
-        .map(|l| {
-            right
-                .iter()
-                // count number of times l appears in right
-                .filter(|r| l == *r)
-                .count() as i32
-        })
-        .sum();
+    let mut count: HashMap<&i32, i32> = HashMap::new();
+    for i in right.iter() {
+        count.entry(i).or_insert(0);
+    }
+    let mut sum: i32 = 0;
+    for l in left.iter() {
+        sum += l * count.get(l).unwrap_or(&0);
+    }
     sum
 }
